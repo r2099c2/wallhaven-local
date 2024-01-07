289f092a47e9524b824dde9e782b6eca8391dad1
rick <r_c2@qq.com>
2024-01-07 18:36:16 +0800

    style: 基本的样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { invoke } from '@tauri-apps/api/tauri';
 import './App.css';
 
 function App() {
+  const [imgs, setImgs] = useState<string[]>();
+
   const fetchData = async () => {
-    const res = await invoke('get_data');
+    const res = await invoke<Array<string>>('get_data');
     console.log(res);
+    setImgs(res);
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <div className="row">
+        {imgs?.map((img, i) => (
+          <div key={i} className="cell">
+            <img src={img} alt="" className="item" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
